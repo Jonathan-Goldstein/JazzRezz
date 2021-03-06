53750e2a8d9e810a71b81452a85830109523338c
Latte <cchukwuani@gmail.com>
2021-03-06 11:02:35 -0500

    added packages server listening

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const PORT = process.env.PORT  || 3001;
@@ -7,3 +10,8 @@ app.use(express.json());
 
 // DATA
 const rezzy = [{}];
+
+// Basic 
+
+
+app.listen(PORT, () => console.log('App listening on Port: 3001'))
